cdfb6827b2a08b45b32de734e2977589ad59ba39
Christoph Rzymski <christoph@functor-argument.net>
2019-07-04 07:49:39 +0200

    Code cleanup, Glottolog 4.0, release 1.0.1.

--- lexibank_castrosui.py
+++ lexibank_castrosui.py
@@ -1,19 +1,17 @@
-from __future__ import unicode_literals, print_function
 from collections import OrderedDict, defaultdict
 
 import attr
-from clldutils.misc import slug
 from clldutils.path import Path
-from clldutils.text import split_text, strip_brackets
-from pylexibank.dataset import Dataset as BaseDataset
-from pylexibank.dataset import Concept, Language
-
 from lingpy import *
+from pylexibank.dataset import Concept, Language
+from pylexibank.dataset import Dataset as BaseDataset
 from tqdm import tqdm
+
 
 @attr.s
 class HConcept(Concept):
     Chinese_Gloss = attr.ib(default=None)
+
 
 @attr.s
 class HLanguage(Language):
@@ -25,88 +23,98 @@ class HLanguage(Language):
 
 
 class Dataset(BaseDataset):
-    id = 'castrosui'
+    id = "castrosui"
     dir = Path(__file__).parent
     concept_class = HConcept
     language_class = HLanguage
-    
+
     def clean_form(self, item, form):
-        return form.strip().replace(' ', '_')
+        return form.strip().replace(" ", "_")
 
     def cmd_download(self, **kw):
         pass
 
     def cmd_install(self, **kw):
 
-        data = self.raw.read_csv('wordlist.tsv', delimiter="\t")
-        langs = {} # need for checking later
-        concepts = OrderedDict([(c.number.rjust(3, '0'), c) for c in self.conceptlist.concepts.values()])
+        wl = self.raw.read_csv("wordlist.tsv", delimiter="\t")
+        langs = {}  # need for checking later
+        concepts = OrderedDict(
+            [(c.number.rjust(3, "0"), c) for c in self.conceptlist.concepts.values()]
+        )
 
         with self.cldf as ds:
 
             for concept in concepts.values():
                 ds.add_concept(
-                        ID=concept.number.rjust(3, '0'),
-                        Name=concept.english,
-                        Chinese_Gloss=concept.attributes['chinese'],
-                        Concepticon_ID=concept.concepticon_id,
-                        Concepticon_Gloss=concept.concepticon_gloss
-                        )
+                    ID=concept.number.rjust(3, "0"),
+                    Name=concept.english,
+                    Chinese_Gloss=concept.attributes["chinese"],
+                    Concepticon_ID=concept.concepticon_id,
+                    Concepticon_Gloss=concept.concepticon_gloss,
+                )
             for language in self.languages:
                 ds.add_language(
-                        ID=language['ID'],
-                        Glottocode=language['Glottolog'],
-                        Name=language['Name'],
-                        SubGroup=language['Group'],
-                        Chinese_name=language['Chinese_name'],
-                        Latitude=language['Latitude'],
-                        Longitude=language['Longitude']
-                        )
-                langs[language['ID']] = language
+                    ID=language["ID"],
+                    Glottocode=language["Glottolog"],
+                    Name=language["Name"],
+                    SubGroup=language["Group"],
+                    Chinese_name=language["Chinese_name"],
+                    Latitude=language["Latitude"],
+                    Longitude=language["Longitude"],
+                )
+                langs[language["ID"]] = language
 
             ds.add_sources(*self.raw.read_bib())
 
             idx = 1
-            D = {0: ['doculect', 'doculectid', 'glottocode', 'concept',
-                'glossid', 'value', 'form', 'phonetic', 'concepticon_id',
-                'concepticon_gloss']}
+            mapping = {
+                0: [
+                    "doculect",
+                    "doculectid",
+                    "glottocode",
+                    "concept",
+                    "glossid",
+                    "value",
+                    "form",
+                    "phonetic",
+                    "concepticon_id",
+                    "concepticon_gloss",
+                ]
+            }
 
-            for line in tqdm(data, desc='load the data'):
+            for line in tqdm(wl, desc="load the data"):
                 if not line[0].strip():
                     phonetic = True
                 if line[0] == "'Ref#":
                     numbers = line
                     phonetic = False
                     idxs = defaultdict(list)
-                elif line[0] == 'Gloss':
+                elif line[0] == "Gloss":
                     glosses = line
                 elif line[0] in langs and not phonetic:
                     taxon = line[0]
-                    for num, gloss, val in zip(
-                            numbers[1:],
-                            glosses[1:],
-                            line[1:]
-                            ):
+                    for num, gloss, val in zip(numbers[1:], glosses[1:], line[1:]):
                         if num.strip() and gloss.strip():
                             cname = concepts[num[1:]]
-                            forms = val.split(',')
+                            forms = val.split(",")
                             if forms:
                                 for form in forms:
-                                    D[idx] = [
-                                            langs[taxon]['Name'], 
-                                            taxon, 
-                                            langs[taxon]['Glottolog'], 
-                                            cname.english, 
-                                            num[1:], 
-                                            val, form.strip(), 
-                                            '', # check later for phonetic value
-                                            cname.concepticon_id, 
-                                            cname.concepticon_gloss,
-                                            ]
+                                    mapping[idx] = [
+                                        langs[taxon]["Name"],
+                                        taxon,
+                                        langs[taxon]["Glottolog"],
+                                        cname.english,
+                                        num[1:],
+                                        val,
+                                        form.strip(),
+                                        "",  # check later for phonetic value
+                                        cname.concepticon_id,
+                                        cname.concepticon_gloss,
+                                    ]
                                     idxs[taxon, gloss] += [idx]
                                     idx += 1
                             else:
-                                print('missing value', gloss, num, taxon)
+                                print("missing value", gloss, num, taxon)
 
                 elif line[0] in langs and phonetic:
                     taxon = line[0]
@@ -116,23 +124,20 @@ class Dataset(BaseDataset):
                             if not these_idx:
                                 pass
                             else:
-                                forms = val.split(',')
+                                forms = val.split(",")
                                 for this_idx, form in zip(these_idx, forms):
-                                    D[this_idx][7] = form
+                                    mapping[this_idx][7] = form
             # export to lingpy wordlist in raw folder
-            Wordlist(D).output('tsv', filename=self.dir.joinpath('raw',
-                'lingpy-wordlist').as_posix())
+            Wordlist(mapping).output(
+                "tsv", filename=self.dir.joinpath("raw", "lingpy-wordlist").as_posix()
+            )
 
             # add data to cldf
-            for idx in tqdm(range(1, len(D)), desc='cldf the data',
-                    total=len(D)):
-                vals = dict(zip(D[0], D[idx]))
+            for idx in tqdm(range(1, len(mapping)), desc="cldf the data", total=len(mapping)):
+                vals = dict(zip(mapping[0], mapping[idx]))
                 ds.add_lexemes(
-                    Language_ID=vals['doculectid'],
-                    Parameter_ID=vals['glossid'],
-                    Value=vals['value'],
-                    Source=['Castro2015']
-                    )
-
-
-
+                    Language_ID=vals["doculectid"],
+                    Parameter_ID=vals["glossid"],
+                    Value=vals["value"],
+                    Source=["Castro2015"],
+                )
--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from setuptools import setup
-import sys
 import json
 
 
-with open('metadata.json', encoding='utf-8') as fp:
+with open('metadata.json') as fp:
     metadata = json.load(fp)
 
 
@@ -21,6 +20,12 @@ setup(
         ]
     },
     install_requires=[
-        'pylexibank>=1.1.1',
-    ]
+        'pylexibank==1.1.1',
+        'segments==2.0.2'
+    ],
+    extras_require={
+        'test': [
+            'pytest-cldf',
+        ],
+    },
 )
