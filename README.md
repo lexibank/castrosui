ad90dc0b04e158e7cab17a0106a9f60c8c1530ed
lingulist <mattis.list@lingpy.org>
2019-10-25 15:31:45 +0200

    update

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ Cite the source dataset as
 
 This dataset is licensed under a https://creativecommons.org/licenses/by-nc/4.0/ license
 
-Conceptlists in Concepticon:
-- [Castro-2015-608](http://concepticon.clld.org/contributions/Castro-2015-608)
 
+Conceptlists in Concepticon:
+- [Castro-2015-608](https://concepticon.clld.org/contributions/Castro-2015-608)
 ## Notes
 
 This dataset was taken from the original data published by Andy Castro, who was so friendly to also share his original concept list with us in digital form. It comprises 16 varieties of the Sui branch of Tai-Kadai, in plain IPA with morphological segmentation.
@@ -20,7 +20,7 @@ This dataset was taken from the original data published by Andy Castro, who was 
 
 [![Build Status](https://travis-ci.org/lexibank/castrosui.svg?branch=master)](https://travis-ci.org/lexibank/castrosui)
 ![Glottolog: 100%](https://img.shields.io/badge/Glottolog-100%25-brightgreen.svg "Glottolog: 100%")
-![Concepticon: 86%](https://img.shields.io/badge/Concepticon-86%25-yellowgreen.svg "Concepticon: 86%")
+![Concepticon: 85%](https://img.shields.io/badge/Concepticon-85%25-yellowgreen.svg "Concepticon: 85%")
 ![Source: 100%](https://img.shields.io/badge/Source-100%25-brightgreen.svg "Source: 100%")
 ![BIPA: 100%](https://img.shields.io/badge/BIPA-100%25-brightgreen.svg "BIPA: 100%")
 ![CLTS SoundClass: 100%](https://img.shields.io/badge/CLTS%20SoundClass-100%25-brightgreen.svg "CLTS SoundClass: 100%")
@@ -31,6 +31,6 @@ This dataset was taken from the original data published by Andy Castro, who was 
 - **Sources:** 1
 - **Synonymy:** 1.04
 - **Invalid lexemes:** 0
-- **Tokens:** 41,984
+- **Tokens:** 41,989
 - **Segments:** 101 (0 BIPA errors, 0 CTLS sound class errors, 101 CLTS modified)
 - **Inventory size (avg):** 67.00
--- lexibank_castrosui.py
+++ lexibank_castrosui.py
@@ -1,19 +1,21 @@
 from collections import OrderedDict, defaultdict
 
 import attr
-from clldutils.path import Path
-from lingpy import *
-from pylexibank.dataset import Concept, Language
+from pathlib import Path
+from pylexibank import Concept, Language
 from pylexibank.dataset import Dataset as BaseDataset
 from pylexibank.util import pb
 
+from lingpy import *
+from clldutils.misc import slug
+
 @attr.s
-class HConcept(Concept):
+class CustomConcept(Concept):
     Chinese_Gloss = attr.ib(default=None)
 
 
 @attr.s
-class HLanguage(Language):
+class CustomLanguage(Language):
     ChineseName = attr.ib(default=None)
     Population = attr.ib(default=None)
     Latitude = attr.ib(default=None)
@@ -29,109 +31,89 @@ class HLanguage(Language):
 class Dataset(BaseDataset):
     id = "castrosui"
     dir = Path(__file__).parent
-    concept_class = HConcept
-    language_class = HLanguage
+    concept_class = CustomConcept
+    language_class = CustomLanguage
 
-    def clean_form(self, item, form):
-        return form.strip().replace(" ", "_")
+    def cmd_makecldf(self, args):
 
-    def cmd_download(self, **kw):
-        pass
-
-    def cmd_install(self, **kw):
-
-        wl = self.raw.read_csv("wordlist.tsv", delimiter="\t")
-        langs = {}  # need for checking later
-        concepts = OrderedDict(
-            [(c.number.rjust(3, "0"), c) for c in self.conceptlist.concepts.values()]
-        )
-
-        with self.cldf as ds:
-
-            for concept in concepts.values():
-                ds.add_concept(
-                    ID=concept.number.rjust(3, "0"),
-                    Name=concept.english,
-                    Chinese_Gloss=concept.attributes["chinese"],
-                    Concepticon_ID=concept.concepticon_id,
-                    Concepticon_Gloss=concept.concepticon_gloss,
-                )
-            langs = {k['ID_in_Source']: k for k in self.languages}
-            ds.add_languages()
-            ds.add_sources(*self.raw.read_bib())
-
-            idx = 1
-            mapping = {
-                0: [
-                    "doculect",
-                    "doculectid",
-                    "glottocode",
-                    "concept",
-                    "glossid",
-                    "value",
-                    "form",
-                    "phonetic",
-                    "concepticon_id",
-                    "concepticon_gloss",
-                ]
-            }
-
-            for line in pb(wl, desc="load the data"):
-                if not line[0].strip():
-                    phonetic = True
-                if line[0] == "'Ref#":
-                    numbers = line
-                    phonetic = False
-                    idxs = defaultdict(list)
-                elif line[0] == "Gloss":
-                    glosses = line
-                elif line[0] in langs and not phonetic:
-                    taxon = line[0]
-                    for num, gloss, val in zip(numbers[1:], glosses[1:], line[1:]):
-                        if num.strip() and gloss.strip():
-                            cname = concepts[num[1:]]
-                            forms = val.split(",")
-                            if forms:
-                                for form in forms:
-                                    mapping[idx] = [
-                                        langs[taxon]["Name"],
-                                        taxon,
-                                        langs[taxon]["Glottocode"],
-                                        cname.english,
-                                        num[1:],
-                                        val,
-                                        form.strip(),
-                                        "",  # check later for phonetic value
-                                        cname.concepticon_id,
-                                        cname.concepticon_gloss,
-                                    ]
-                                    idxs[taxon, gloss] += [idx]
-                                    idx += 1
-                            else:
-                                print("missing value", gloss, num, taxon)
-
-                elif line[0] in langs and phonetic:
-                    taxon = line[0]
-                    for gloss, val in zip(glosses[1:], line[1:]):
-                        if gloss.strip():
-                            these_idx = idxs.get((taxon, gloss))
-                            if not these_idx:
-                                pass
-                            else:
-                                forms = val.split(",")
-                                for this_idx, form in zip(these_idx, forms):
-                                    mapping[this_idx][7] = form
-            # export to lingpy wordlist in raw folder
-            Wordlist(mapping).output(
-                "tsv", filename=self.dir.joinpath("raw", "lingpy-wordlist").as_posix()
+        wl = self.raw_dir.read_csv("wordlist.tsv", delimiter="\t")
+        concept_lookup = {}
+        for concept in self.conceptlist.concepts.values():
+            idx = concept.id.split('-')[-1]+'_'+slug(concept.english)
+            args.writer.add_concept(
+                ID=idx,
+                Name=concept.english,
+                Chinese_Gloss=concept.attributes["chinese"],
+                Concepticon_ID=concept.concepticon_id,
+                Concepticon_Gloss=concept.concepticon_gloss
             )
+            concept_lookup[concept.number.rjust(3, "0")] = [idx,
+                    concept]
+        language_lookup = {k['ID_in_Source']: k for k in self.languages}
+        args.writer.add_languages()
+        args.writer.add_sources()
 
-            # add data to cldf
-            for idx in pb(range(1, len(mapping)), desc="cldfify", total=len(mapping)):
-                vals = dict(zip(mapping[0], mapping[idx]))
-                ds.add_lexemes(
-                    Language_ID=langs[vals["doculectid"]]['ID'],
-                    Parameter_ID=vals["glossid"],
-                    Value=vals["value"],
-                    Source=["Castro2015"],
-                )
+        idx = 1
+        mapping = {0: ["doculect", "doculectid", "glottocode", "concept",
+            "glossid", "value", "form", "phonetic", "concepticon_id",
+            "concepticon_gloss"]}
+
+        for line in pb(wl, desc="load the data"):
+            if not line[0].strip():
+                phonetic = True
+            if line[0] == "'Ref#":
+                numbers = line
+                phonetic = False
+                idxs = defaultdict(list)
+            elif line[0] == "Gloss":
+                glosses = line
+            elif line[0] in language_lookup and not phonetic:
+                taxon = line[0]
+                for num, gloss, val in zip(numbers[1:], glosses[1:], line[1:]):
+                    if num.strip() and gloss.strip():
+                        cname = concept_lookup[num[1:]][1]
+                        forms = val.split(",")
+                        if forms:
+                            for form in forms:
+                                mapping[idx] = [
+                                    language_lookup[taxon]["Name"],
+                                    taxon,
+                                    language_lookup[taxon]["Glottocode"],
+                                    cname.english,
+                                    num[1:],
+                                    val,
+                                    form.strip(),
+                                    "",  # check later for phonetic value
+                                    cname.concepticon_id,
+                                    cname.concepticon_gloss,
+                                ]
+                                idxs[taxon, gloss] += [idx]
+                                idx += 1
+                        else:
+                            print("missing value", gloss, num, taxon)
+
+            elif line[0] in language_lookup and phonetic:
+                taxon = line[0]
+                for gloss, val in zip(glosses[1:], line[1:]):
+                    if gloss.strip():
+                        these_idx = idxs.get((taxon, gloss))
+                        if not these_idx:
+                            pass
+                        else:
+                            forms = val.split(",")
+                            for this_idx, form in zip(these_idx, forms):
+                                mapping[this_idx][7] = form
+        # export to lingpy wordlist in raw folder
+        #Wordlist(mapping).output(
+        #    "tsv", filename=self.dir.joinpath("raw", "lingpy-wordlist").as_posix()
+        #)
+
+        # add data to cldf
+        for idx in pb(range(1, len(mapping)), desc="cldfify", total=len(mapping)):
+            vals = dict(zip(mapping[0], mapping[idx]))
+            args.writer.add_lexemes(
+                Language_ID=language_lookup[vals["doculectid"]]['ID'],
+                Parameter_ID=concept_lookup[vals["glossid"]][0],
+                Value=vals["value"],
+                Source=["Castro2015"],
+            )
